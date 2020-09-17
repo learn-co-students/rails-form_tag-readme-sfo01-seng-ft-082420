dfe41bc9a7353de1ab57d4d99a8bc9f53bdd2630
kendallstephens <kendoll.stephens@gmail.com>
2020-09-17 11:28:44 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ 
+Post.create(title: "Rail Forms", description: "Blah, Blah, Blah")
+Post.create(title: "CRUD", description: "Create, Read, Update, Delete")
